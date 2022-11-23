c1b7f96ee3fe86a4ecb93f5c62834c332d09d1de
AARON <90492963+aarongeo1@users.noreply.github.com>
2022-11-23 10:52:39 -0700

    Add files via upload

---
+++ load-json.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+import json
+import subprocess
+
+
+def connect(jfile,port = ""):
+    client = MongoClient(port)
+    db = client["291db"]
+    collection = db["dblp"]
+    p = subprocess.Popen("mongoimport --db 291db --collection dblp --drop --batchSize 1 --file ./{}".format(jfile), stdout=subprocess.PIPE, shell=True)
+    
+
+
+if __name__ == "__main__":
+    jfile = input("Enter the json file")
+    port = input("Ënter the post number")
+    connect(jfile,port)
